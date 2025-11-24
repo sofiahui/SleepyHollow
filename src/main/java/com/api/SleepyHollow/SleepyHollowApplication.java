00a686c2f5ed9463317ad712b25eb9a7df631773
sofiahuichulef <so.huichulef@duocuc.cl>
2025-11-24 18:10:50 -0300

    first commit

---
+++ src/main/java/com/api/SleepyHollow/SleepyHollowApplication.java
@@ -0,0 +1,13 @@
+package com.api.SleepyHollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepyHollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepyHollowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/SleepyHollow/SleepyHollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.SleepyHollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepyHollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
